0a3f56d9c04201be3cb4383d27fc085ab2001042
kimi <gang.lvg@alibaba-inc.com>
2013-04-17 16:07:11 +0800

    turning

--- c/reverse_ipv4.c
+++ c/reverse_ipv4.c
@@ -6,27 +6,43 @@
 
 static char* reverse_ipv4(char *ip)
 {
+    int pos = 0;
+    int dot_num = 0;
     if (NULL == ip)
     {
         return ip;
+    }
+    for(pos = 0; *(ip + pos) != '\0'; pos++)
+    {
+        if (isdigit(*(ip + pos)) != 0)
+        {
+            continue;
+        } 
+        else if ('.' == *(ip + pos))
+        {
+            dot_num++;
+        }
+        else 
+        {
+            return NULL;
+        }
+    }
+
+    if (3 != dot_num)
+    {
+        return NULL;
     }
 
     int len = strlen(ip);
     char *result = malloc(len + 1);
     *(result + len) = '\0';
     int start = 0;
-    int pos = 0;
+    pos = 0;
     int rev_pos = len;
-    int dot_num = 0;
+    dot_num = 0;
     int ch_num = 0;
     while(dot_num < 3)
     {
-        if (isdigit(*(ip + pos)) == 0 && '.' != *(ip + pos))
-        {
-            free(result);
-            return NULL;
-        }
-        
         if ('.' == *(ip + pos)) {
             strncpy(result + rev_pos, ip + start, ch_num);
             rev_pos--;
@@ -53,4 +69,5 @@ static char* reverse_ipv4(char *ip)
 int main(int argc, char *argv)
 {
     printf("%s\n", reverse_ipv4("12.34.56.78"));
+    printf("%s\n", reverse_ipv4("1.3.5.7"));
 }
